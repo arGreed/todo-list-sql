33a9b38549647c582641e26dedb7d087345444bf
Artem <arcelsium@gmail.com>
2025-02-13 22:22:15 +0300

    завершение структуры базы данных

--- db.sql
+++ db.sql
@@ -16,7 +16,6 @@ comment on column to_do_list.role.valid_to is 'Дата и время оконч
 comment on column to_do_list.role.code is 'Код роли';
 comment on column to_do_list.role.name is 'Наименование роли';
 
-
 create table to_do_list.user (
 	id			bigserial primary key,
 	created_at	timestamp default localtimestamp not null,
@@ -25,25 +24,50 @@ create table to_do_list.user (
 	login		varchar(100) unique not null,
 	email		varchar(100) unique not null,
 	password	varchar(100) not null,
-	foreign key (role_code) references to_do_list.role (code) on delete no action
+	foreign key (role_code) references to_do_list.role (code)
 );
 
 comment on table to_do_list.user is 'Перечень пользователей приложения';
 
 comment on column to_do_list.user.id is 'Идентификатор пользователя';
-comment on column to_do_list.user.created_at is 'Дата и время регистрации пользователя';
+comment on column to_do_list.user.created_at is 'Дата и время создания пользователя';
 comment on column to_do_list.user.role_code is 'Код роли пользователя';
 comment on column to_do_list.user.name is 'Имя пользователя';
 comment on column to_do_list.user.login is 'Логин пользователя';
 comment on column to_do_list.user.email is 'Электронная почта пользователя';
 comment on column to_do_list.user.password is 'Пароль пользователя';
 
-create table note_type (
+create table to_do_list.note_type (
 	id			bigserial primary key,
 	created_at	timestamp default localtimestamp not null,
-	creator		int8 not null,
-	name		varchar(100) unique,
-	foreign key (creator) references to_do_list.user (id)
+	creator_id	int8 not null,
+	name		varchar(100) not null,
+	foreign key (creator_id	) references to_do_list.user (id)
+);
+
+comment on table to_do_list.note_type is 'Перечень типов заметок';
+
+comment on column to_do_list.note_type.id is			'Идентификатор типа заметки';
+comment on column to_do_list.note_type.created_at is	'Дата и время создания типа заметки';
+comment on column to_do_list.note_type.creator_id is	'Создатель типа заметки';
+comment on column to_do_list.note_type.name	 is			'Наименование типа заметки';
+
+create table to_do_list.note (
+	id				bigserial primary key,
+	created_at		timestamp default localtimestamp not null,
+	creator_id		int8 not null,
+	type			int8 not null,
+	content			text not null,
+	is_completed	bool not null default false,
+	foreign key (creator_id) references to_do_list.user (id),
+	foreign key (type) references to_do_list.note_type (id)
 )
 
-comment
+comment on table to_do_list.note is 'Перечень заметок'
+
+comment on column to_do_list.note.id is 'Идентификатор заметки';
+comment on column to_do_list.note.created_at is 'Дата и время создания заметки';
+comment on column to_do_list.note.creator_id is 'Идентификатор автора заметки';
+comment on column to_do_list.note.type is 'Тип заметки';
+comment on column to_do_list.note.content is 'Содержание заметки';
+comment on column to_do_list.note.is_completed is 'Признак актуальности заметки';
